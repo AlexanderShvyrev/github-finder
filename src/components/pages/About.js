5a4041d85078b136b218fe68a7ba3c30fb3a5d68
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-05-11 23:36:33 -0500

    initial commit
    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this App</h1>
+            <h3>App to search Github users</h3>
+            <h4>Version: 1.0.0</h4>
+        </Fragment>
+    )
+}
+export default About;
